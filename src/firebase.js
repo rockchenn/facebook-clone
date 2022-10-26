7d4a2a64daac7293084bb8c70a4dac4a89505067
Rock Chen <rockchen.tw@gmail.com>
2022-10-26 11:02:48 +0800

    Add firebase configuration file.

---
+++ src/firebase.js
@@ -0,0 +1,36 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore, collection } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: ,
+  measurementId: 
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+const colRef = collection(db, Your collection name);
+
+// Initialize Cloud Storage and get a reference to the service
+const storage = getStorage(app);
+
+export { auth, colRef, storage };
